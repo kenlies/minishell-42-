94d3295372d2831b0de04229e0e9deb9978197b8
alexnda <97135325+kenlies@users.noreply.github.com>
2023-11-08 13:19:03 +0200

    Add files via upload

---
+++ uwu/uwu_two.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   uwu_two.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aotsala <aotsala@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 16:39:51 by aotsala           #+#    #+#             */
+/*   Updated: 2023/08/20 21:41:21 by aotsala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	print_top_33(void)
+{
+	printf("@..../____  >......................#GPPP55555555555555555PG*");
+	printf("#..#5555555555PPPPPPPPGGG**#####************GGGGGGGGGGPP5555");
+	printf("555PPG********@\n");
+	usleep(16969);
+	printf("@.........\\/....................#*GPP5555555555555555555555");
+	printf("555PG*...PY55P55G...........###************GGGGGGGGGGGGGGGGP");
+	printf("5555PPG********@\n");
+	usleep(16969);
+	printf("@............................#*PPP55555555555555555555555555");
+	printf("55555GPYY...PG..........#####************GGGGGGGGGGGGGGGGGP5");
+	printf("555PP*********@\n");
+	usleep(16969);
+	printf("@.-.........................#GPPP555555555555555555555555555");
+	printf("55555555555P#*GP#......#######***********GGGGGGGGGGGGGGGGGP5");
+	printf("555PP*********@\n");
+	usleep(16969);
+}
+
+void	print_top_3(void)
+{
+	print_top_33();
+	printf("@........................#*GPP555555555555555555555555555555");
+	printf("55555555555G*G5P#.....#########*********GGGGGGGGGGGGGGGG*GP5");
+	printf("555PPG********@\n");
+	usleep(16969);
+	printf("@......-...............*GPP555555555555555555555555555555555");
+	printf("555555555PPPPGP55P*##..#######********GGGGGGGGGGGGGGG****GP5");
+	printf("5555PG********@\n");
+	usleep(16969);
+	printf("@-........-........-#*PPP55555555555555555555555555555555555");
+	printf("555555555555555PPP5PPG*#######********GGGGGGGGGGGGG******GP5");
+	printf("5555PP********@\n");
+	usleep(16969);
+	printf("@.-.....-......-..#GPP5555555555555555555555555555555555555Y");
+	printf("YYYYYYYY5G5Y5PPPPPP555PPPGG**********GGGGGGGGGGGG********PP5");
+	printf("5555PP********@\n");
+	usleep(16969);
+}
+
+void	print_top_44(void)
+{
+	printf("@--....-....-..#*PPP5555555555555555555555PP55555555555YYYYY");
+	printf("YYYYYYYJ5@.**P5PP555PP555555PPG******GGGGGGGGGG**********PP5");
+	printf("5555PPG*******@\n");
+	usleep(16969);
+	printf("@.--..-...-..#GP5555555555555555555555PGG55P555555555YYYYYYY");
+	printf("YYYYYYYY*@.@@*YYY5P*#.*P5Y55555PGGG*GGGGGGGG*************P55");
+	printf("5555PPG*******@\n");
+	usleep(16969);
+	printf("@--..-...-#GP5555555555555555555555PG#.@*5555555555YYYYYYYYY");
+	printf("YYYYYYY5.@..@.5YYYY5G.@.#PYYY555555PGGGGGG**************GP55");
+	printf("55555PP*******@\n");
+	usleep(16969);
+	printf("@--..-.-*P55555555555555555555PPPG#..@@@#555555555YYYYYYYYYY");
+	printf("Y5YYYYYP.@...@*YYYYYY5G.@@#PYYPYYYY55PPG***************#GP55");
+	printf("55555PPG******@\n");
+	usleep(16969);
+}
+
+void	print_top_4(void)
+{
+	print_top_44();
+	printf("@----*G55555555555555555555PPP*#.......@.555555555YYYYYYYYYY");
+	printf("YYYYYYYG@....@.5YYYYYYY5*...*555JYYYY555PG*************#PP55");
+	printf("55555PPG***###@\n");
+	usleep(16969);
+	printf("@-#GP5555555555555555555PPG*#..........@.P555555P5YYYYYYYYYY");
+	printf("YJJJJJ5.@.....@GYYYYYYJJJ5*..#*PYJJYYY5555PGG***********PP55");
+	printf("55555PPG**####@\n");
+	usleep(16969);
+	printf("@G5555555555555555555PPG*#..............@G555555P5Y55555YYYY");
+	printf("YYYYY5.@......@#YYYYYJJJJJJP####G5JYYYY55555PPG*******#*P555");
+	printf("55555PPP*#####@\n");
+	usleep(16969);
+	printf("@5555555555555555PPPG*#...............@@@*55555555Y555555YY5");
+	printf("5555G.@........#5YYYYYYJJJJJYG####PYYYY55555555PPGG***#GP555");
+	printf("55555PPP*#####@\n");
+	usleep(16969);
+}
